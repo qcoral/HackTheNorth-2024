fad085a408049094f5a3aede125d60ff6c71de1e
acon <101356372+acornitum@users.noreply.github.com>
2024-09-14 12:57:23 -0400

    added initial version of game

---
+++ htn2024/scripts/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const speed = 200
+
+var current_dir = "none"
+
+func _physics_process(delta):
+	player_movement(delta) 
+
+func player_movement(delta):
+	
+	if Input.is_action_pressed("right"):
+		velocity.x = speed
+		velocity.y = 0
+		current_dir = "right"
+		
+		
+	elif Input.is_action_pressed("left"):
+		velocity.x = -speed
+		velocity.y = 0
+		current_dir = "left"
+	
+	elif Input.is_action_pressed("up"):
+		velocity.x = 0
+		velocity.y = -speed
+		current_dir = "up"
+		
+	elif Input.is_action_pressed("down"):
+		velocity.x = 0
+		velocity.y = speed
+		current_dir = "down"
+	
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
